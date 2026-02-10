cf05145237988181e9abc62d2f188d21b95fb9cb
Saheer <saheerdesignn@gmail.com>
2026-02-09 16:53:18 -0800

    use extra migration files

--- execute_fix.js
+++
@@ -1,2 +0,0 @@
-// placeholder for execute_fix.js
-console.log('execute_fix placeholder - restore original script content if available');
--- generate_fix_sql.js
+++
@@ -1,2 +0,0 @@
-// placeholder for generate_fix_sql.js
-console.log('generate_fix_sql placeholder - restore original script content if available');
